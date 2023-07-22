5404388d7e39bc5ea4e495be9371f3b5003d840e
Henrique Silva <104944100+Henriquesb-1@users.noreply.github.com>
2023-07-21 23:33:18 -0300

    Update Home.tsx
    Add theme change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
+import ChangeTheme from "../components/template/changeTheme/ChangeTheme";
 import Footer from "../components/template/footer/Footer";
 import HomeTemplate from "../components/template/home/HomeTemplate";
+import useTheme from "../context/hook/useTheme";
+
+import "./pages.css";
 
 export default function Home() {
+    const { theme } = useTheme();
+
     return (
-        <>
+        <main className={`App ${theme}`} style={{backgroundColor: theme === "dark" ? "#111": "#aaa"}}>
+            <ChangeTheme />
             <HomeTemplate />
             <Footer />
-        </>
+        </main>
     )
 }
